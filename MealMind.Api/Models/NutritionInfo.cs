ce5ca9498d7858ca0c2a45ca8d0245c7184199ff
Mahan Mashoof <mahan.mashoof@gmail.com>
2026-07-14 21:11:12 +0200

    added validation for model attributes (#7)
    added global error handling

--- MealMind.Api/Models/NutritionInfo.cs
+++ MealMind.Api/Models/NutritionInfo.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MealMind.Api.Models;
 
 public class NutritionInfo
 {
+    [Range(0, 10000)]
     public int Calories { get; set; }
+    [Range(0, 1000)]
     public double ProteinGrams { get; set; }
+    [Range(0, 1000)]
     public double CarbsGrams { get; set; }
+    [Range(0, 1000)]
     public double FatGrams { get; set; }
 }
--- MealMind.Api/Models/Recipe.cs
+++ MealMind.Api/Models/Recipe.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MealMind.Api.Models
 {
     public class Recipe
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
         public string? ImageUrl { get; set; }
         public NutritionInfo? Nutrition { get; set; }
