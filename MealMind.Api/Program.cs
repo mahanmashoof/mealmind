f43ebcda92e6b05f068ba2cb1da014029928dc2a
Mahan Mashoof <mahan.mashoof@gmail.com>
2026-08-30 13:31:05 +0200

    added vercel fe url to cors

--- MealMind.Api/Program.cs
+++ MealMind.Api/Program.cs
@@ -72,7 +72,7 @@ builder.Services.AddHostedService<PrepReminderBgService>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "https://mealmind-virid.vercel.app/")
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
